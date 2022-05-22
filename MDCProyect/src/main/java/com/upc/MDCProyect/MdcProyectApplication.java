fb115e49d609f5d09c9da4d8f48ec8bd36bdeaec
LeonardoSamaniego <84730758+LeonardoSamaniego@users.noreply.github.com>
2022-05-21 20:22:27 -0500

    Initial project
    Creacion de proyecto - Version 1.0

---
+++ MDCProyect/src/main/java/com/upc/MDCProyect/MdcProyectApplication.java
@@ -0,0 +1,13 @@
+package com.upc.MDCProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdcProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdcProyectApplication.class, args);
+	}
+
+}
---
+++ MDCProyect/src/test/java/com/upc/MDCProyect/MdcProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.MDCProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
