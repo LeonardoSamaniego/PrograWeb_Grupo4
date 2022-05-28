cf48da41afa3d09db8a3254234b137e1941628a0
CarlosAlv1201 <u201916826@upc.edu.pe>
2022-05-27 21:04:36 -0500

    ultimate

--- MDCProyect/src/main/java/com/example/repository/UserRepository.java
+++ MDCProyect/src/main/java/com/example/repository/UserRepository.java
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 package com.example.repository;
 
 
@@ -12,6 +13,21 @@ public interface UserRepository  extends JpaRepository<Users, Long> {
 
 	public Users findByUsername(String username);
 }
+=======
+package com.example.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entities.Users;
+
+@Repository
+public interface UserRepository  extends JpaRepository<Users, Long> {
+
+	public Users findByUsername(String username);
+}
+>>>>>>> Stashed changes
 =======
 package com.example.repository;
 
--- MDCProyect/src/main/java/com/example/service/UserService.java
+++ MDCProyect/src/main/java/com/example/service/UserService.java
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 package com.example.service;
 
 import java.util.List;
@@ -20,6 +21,29 @@ public interface UserService {
 	Users findByUsername(String username);
 
 }
+=======
+package com.example.service;
+
+import java.util.List;
+
+import com.example.entities.Users;
+
+public interface UserService {
+
+	Users insert(Users user);
+
+	Users update(Users user);
+
+	Users getById(Long id);
+
+	void delete(Long id);
+
+	List<Users> findAll();
+
+	Users findByUsername(String username);
+
+}
+>>>>>>> Stashed changes
 =======
 package com.example.service;
 
